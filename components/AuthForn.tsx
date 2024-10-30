767fc99d48d32323fcab6a7b16056f957ca57c2f
Austin Okonkwo <austinokonkwo1995@gmail.com>
2024-10-30 21:53:55 +0100

    Implement PlaidLink and Dwolla

--- components/AuthForn.tsx
+++ components/AuthForn.tsx
@@ -15,6 +15,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { signIn, signUp } from '@/lib/actions/user.action'
+import PlaidLink from './PlaidLink'
 
 
 const AuthForn = ({ type }: { type: string}) => {
@@ -41,7 +42,20 @@ const AuthForn = ({ type }: { type: string}) => {
       // Sign up with Appwrite & create plaid token
 
       if(type === 'sign-up') {
-        const newUser = await signUp(data)
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+
+        const newUser = await signUp(userData)
 
         setUser(newUser);
       }
@@ -89,7 +103,7 @@ const AuthForn = ({ type }: { type: string}) => {
       </header>
       {user ? (
         <div className='flex flex-col gap-4'>
-          {/* PlaidLink */}
+          <PlaidLink user={user} variant='primary' />
         </div>
       ): (
         <>
